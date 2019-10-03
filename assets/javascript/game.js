23efc4e1c05777d629e6462a4bc197d364d7b2d3
dan demeo <chronos937@gmail.com>
2019-10-03 14:16:30 -0400

    cleaned up js unable to update guesses with keyup so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,50 @@
 var letterBox = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-var cpuChoice = Math.floor(Math.random(letterBox) * letterBox.length);
+var winText = document.getElementById("winTracker");
+var lossText = document.getElementById("lossTracker");
+var guessText = document.getElementById("guessTracker");
+var triesText = document.getElementById("guessesLeft");
 
+var wins = 0;
+var losses = 0;
+var tries = 9;
+var guesses = "";
+
+var cpuChoice = Math.floor(Math.random(letterBox) * letterBox.length);
 console.log(letterBox[cpuChoice]);
+
 
 document.onkeyup = function(event) {
     var pcGuess = event.key;
     var lowerPcGuess = pcGuess.toLowerCase();
-    var wins = 0;
     
     console.log(lowerPcGuess);
 
 
     if(lowerPcGuess === cpuChoice) {
-        wins ++
-       document.getElementById("winTracker").textContent(wins) 
-       
+        wins++;
+        console.log(wins); 
+        var cpuChoice = Math.floor(Math.random(letterBox) * letterBox.length);
+        console.log(letterBox[cpuChoice]);
+ 
+
+                  
+    }
+    else(lowerPcGuess !== cpuChoice); {
+        tries--;
+        guesses.append(lowerPcGuess);
+        console.log(tries);
+    }
+
+    if(tries === 0) {
+        losses++;
+        var cpuChoice = Math.floor(Math.random(letterBox) * letterBox.length);
+        console.log(letterBox[cpuChoice]);
+
     }
 }
+
+winText.textContent = "Wins: " + wins;
+lossText.textContent = "Losses: " + losses;
+triesText.textContent = "Attempts: " + tries;
+guessText.textContent = "Guesses you have already made: " + guesses;
