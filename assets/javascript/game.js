3877d32419e0d576449376825816fe19fb51f105
dan demeo <chronos937@gmail.com>
2019-10-15 14:03:34 -0400

    fixed issue with losing letter of previous game appearing as a tried letter in next game and cleaned up the code removing unnecessary spacing and console logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var letterBox = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var letterBox = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var winText = document.getElementById("winTracker");
 var lossText = document.getElementById("lossTracker");
@@ -11,50 +11,27 @@ var cpuChoice;
 var tries = 9;
 var guesses = [];
 
-
-//  console.log(cpuChoice);
-
-
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var pcGuess = event.key;
     var lowerPcGuess = pcGuess.toLowerCase();
-    
-    // console.log(lowerPcGuess);
-    // console.log(cpuChoice);
 
-
-
-    if(lowerPcGuess === cpuChoice) {
+    if (lowerPcGuess === cpuChoice) {
         wins++;
-        // console.log(wins); 
         startGame()
-
-        // cpuChoice = Math.floor(Math.random(letterBox) * letterBox.length);
-        // console.log(letterBox[cpuChoice]);
- 
-
-                  
     }
-    else if(guesses.includes(lowerPcGuess)) {
+    else if (guesses.includes(lowerPcGuess)) {
         alert("You've already tried that")
     }
-    
     else {
         tries--;
         guesses.push(lowerPcGuess);
-        // console.log(tries);
     }
-
-    if(tries === 0) {
+    if (tries === 0) {
         losses++;
         startGame()
-        // cpuChoice = Math.floor(Math.random(letterBox) * letterBox.length);
-        // console.log(letterBox[cpuChoice]);
         tries = 9;
-        guesses.push(lowerPcGuess);
-        
-
     }
+
     winText.textContent = "Wins: " + wins;
     lossText.textContent = "Losses: " + losses;
     triesText.textContent = "Attempts: " + tries;
@@ -70,7 +47,6 @@ function startGame() {
     cpuChoice = letterBox[Math.floor(Math.random() * letterBox.length)];
     tries = 9;
     guesses = [];
-    // console.log(cpuChoice);
 }
 
 startGame()
